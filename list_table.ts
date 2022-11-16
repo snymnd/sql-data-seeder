6fd8fe4e20c680f9e00b13a1c1df996df7291fa5
dikiogres <dikiogres@gmail.com>
2022-11-16 23:15:49 +0700

    feat: add pegawai table

--- list_table.ts
+++ list_table.ts
@@ -5,3 +5,16 @@ faker.locale = 'id_ID';
 
 const i = 0;
 
+const pegawai = {
+    pg_id: i,
+    pg_nama: faker.name.findName(),
+    pg_nik: faker.phone.phoneNumber('################'),
+    pg_alamat: faker.address.streetAddress(),
+    pg_no_telp: faker.phone.phoneNumber('+628#########'),
+    pg_password: faker.internet.password(),
+    bagian: faker.random.arrayElement([
+        'BAGIAN 1',
+        'BAGIAN 2',
+        'BAGIAN 3',
+      ]),
+}
